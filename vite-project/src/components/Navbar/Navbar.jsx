ecb5d3f84f3584c1b928d55e4eeb2dec4f91e0a2
fede <ricardofedericovega@gmail.com>
2024-07-20 16:05:38 -0300

    se realizo la estructura completa del navbar

--- vite-project/src/components/Navbar/Navbar.jsx
+++ vite-project/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,41 @@
 import './Navbar.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faBars} from "@fortawesome/free-solid-svg-icons"
+import { useState } from 'react';
 
-function Navbar (){
-
+const Navbar = () =>{
+    
+    const[activeLink, setActiveLink] = useState('portfolio')
+    
     return(
-        <div>
+         <nav className="navbar navbar-expand-lg navbar-light">
             
-        </div>
+                <a className="navbar-brand" href="#"><h2 className='logo'>FDR-RC</h2></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{color:"white"}}/>
+                </button>
 
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#home">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#servicios">Services</a>
+                    </li>
+                    </ul>
+                </div>
+                <button type="button" class="btn btn-primary" href="#contacto">Contacto</button>
+            
+        </nav>
     )
 }
 
 export default Navbar
+
