af4463a2d95002253a68df82fa0b1ed175f85596
fede <ricardofedericovega@gmail.com>
2024-07-20 16:11:00 -0300

    se completo la estructura hero

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className='App'>
        <Navbar/>
+       <Hero/>
     </div>
   )
 }
---
+++ vite-project/src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import "./Hero.css";
+
+function Hero(){
+    return(
+            <div id="hero">
+                <h2 id="tit-1">Hola amigos soy</h2>
+                <h1 id="tit-2">FEDERICO VEGA</h1>
+                <h2 id="tit-3">UI/UX DESIGNER</h2>
+                <h2 id="tit-4">& EMPLOYEE AT</h2>
+                <h2 id="tit-5">XYZ IT SOLUTIONS</h2>
+                <button type="button" class="btn-hero btn-primary">Empezar</button>
+            </div>
+        )
+}
+
+export default Hero;
